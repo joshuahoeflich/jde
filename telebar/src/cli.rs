784303578a6aa4c5705f98344f15988a10ae7734
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-08 21:56:26 -0500

    TODO: Finish parsing the toml map

--- telebar/src/cli.rs
+++ telebar/src/cli.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 
 pub struct InputData {
     pub socket_addr: String,
-    pub config: String,
+    pub config: i32,
 }
 
 #[derive(Debug, PartialEq)]
@@ -17,25 +17,9 @@ pub enum CliParseError {
 
 pub fn parse_cli_args() -> Result<InputData, CliParseError> {
     let matches = get_app_matches();
-    let socket_addr = get_socket_addr(matches.value_of("id").unwrap_or("0").to_owned())?;
-
-    let config = std::fs::read_to_string(get_config_path(matches.value_of("config"))?)
-        .map_err(|_| CliParseError::ConfigFile)?;
-    let toml: toml::Value = toml::from_str(&config).map_err(|_| CliParseError::TomlParseError)?;
-    for i in toml.as_table().unwrap().keys() {
-        println!("i is {}", i);
-    }
-    // println!("{:?}", toml.as_table());
-    // for i in toml.as_array() {
-    //     println!("I RAN");
-    //     for j in i {
-    //         println!("THING IS {}", j);
-    //     }
-    // }
-    println!("THIS RAN");
     Ok(InputData {
-        socket_addr,
-        config,
+        socket_addr: get_socket_addr(matches.value_of("id").unwrap_or("0").to_owned())?,
+        config: get_config(matches.value_of("config"))?,
     })
 }
 
@@ -61,12 +45,17 @@ fn get_app_matches<'a>() -> ArgMatches<'a> {
         .get_matches()
 }
 
-pub fn get_socket_addr(socket_addr: String) -> Result<String, CliParseError> {
-    let xdg_runtime_dir = env::var("XDG_RUNTIME_DIR").map_err(|_| CliParseError::XdgRuntime)?;
-    let mut socket_buffer = PathBuf::new();
-    socket_buffer.push(xdg_runtime_dir);
-    socket_buffer.push(format!("{}_telebar_socket", socket_addr));
-    Ok(socket_buffer.to_string_lossy().into_owned())
+pub fn get_config(maybe_cli_str: Option<&str>) -> Result<i32, CliParseError> {
+    let _config_toml = get_config_toml(maybe_cli_str)?;
+    Ok(0)
+}
+
+pub fn get_config_toml(maybe_cli_str: Option<&str>) -> Result<toml::Value, CliParseError> {
+    toml::from_str(
+        &std::fs::read_to_string(get_config_path(maybe_cli_str)?)
+            .map_err(|_| CliParseError::ConfigFile)?,
+    )
+    .map_err(|_| CliParseError::TomlParseError)
 }
 
 pub fn get_config_path(maybe_cli_str: Option<&str>) -> Result<PathBuf, CliParseError> {
@@ -88,14 +77,10 @@ pub fn get_config_path(maybe_cli_str: Option<&str>) -> Result<PathBuf, CliParseE
     )
 }
 
-pub fn parse_config_table(maybe_cli_str: Option<&str>) -> Result<Vec<String>, CliParseError> {
-    let toml_raw: toml::Value = toml::from_str(
-        &std::fs::read_to_string(get_config_path(maybe_cli_str)?)
-            .map_err(|_| CliParseError::ConfigFile)?,
-    )
-    .map_err(|_| CliParseError::TomlParseError)?;
-    toml_raw
-        .as_table()
-        .ok_or_else(|| CliParseError::TomlParseError)?;
-    Ok(vec![])
+pub fn get_socket_addr(socket_addr: String) -> Result<String, CliParseError> {
+    let xdg_runtime_dir = env::var("XDG_RUNTIME_DIR").map_err(|_| CliParseError::XdgRuntime)?;
+    let mut socket_buffer = PathBuf::new();
+    socket_buffer.push(xdg_runtime_dir);
+    socket_buffer.push(format!("{}_telebar_socket", socket_addr));
+    Ok(socket_buffer.to_string_lossy().into_owned())
 }
