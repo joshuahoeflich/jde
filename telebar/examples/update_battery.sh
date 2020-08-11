97383305843f2d77481d08016afc01dcc7f1bb96
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-10 20:49:03 -0500

    Minor updates to Makefile, shell

--- telebar/examples/update_battery.sh
+++ telebar/examples/update_battery.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./config.sh
 
-printf "%s\n%s" "battery" "Updating the battery!" | nc -U "$XDG_RUNTIME_DIR"/"$TELEBAR_SERVER_ID"_telebar_socket -w0
+printf "%s\n%s" "battery" "Battery update!" | nc -U "$XDG_RUNTIME_DIR"/"$TELEBAR_SERVER_ID"_telebar_socket -w0
