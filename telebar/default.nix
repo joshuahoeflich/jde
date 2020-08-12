95ab28c824511c1d64b939694f6548f66ec98a68
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-12 10:40:50 -0500

    Removes Makefile

--- telebar/default.nix
+++ telebar/default.nix
@@ -2,11 +2,12 @@
 
 pkgs.stdenv.mkDerivation {
   name = "telebar_server";
-  buildInputs = with pkgs; [ rustc cargo ];
+  buildInputs = with pkgs; [ rustc cargo cacert curl ];
   prePatch = ''
     sed -i "s@/usr/local@$out@" config.mk
   '';
   preConfigure = ''
+    curl https://github.com/rust-lang/crates.io-index
     export HOME=$(mktemp -d)
   '';
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
