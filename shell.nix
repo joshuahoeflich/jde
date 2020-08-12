8dca22712e6d750524f57ceb3cc01e996da53f0e
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-12 13:09:09 -0500

    More Cargo artifact removal

--- shell.nix
+++ shell.nix
@@ -2,10 +2,8 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    rustup
     shellcheck
     pre-commit
-    socat
   ];
   shellHook = ''
     if [ -d "$PWD"/.git ] && [ ! -f "$PWD"/.git/hooks/pre-commit ]; then
