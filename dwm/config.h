2363e0dc8dadd6b89558a2d53caafdfc28838214
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-17 22:00:34 -0500

    Fixes dwm fonts

--- dwm/config.h
+++ dwm/config.h
@@ -4,8 +4,8 @@ static const int gappx              = 10;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FuraCode Nerd Font:size=16" };
+static const char dmenufont[]       = "FuraCode Nerd Font:size=16";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
