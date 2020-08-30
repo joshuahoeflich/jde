6608508b76b5b59607490ed14cb29ce5e2e39207
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-30 10:38:58 -0500

    More minor dwm tweaks

--- dwm/config.h
+++ dwm/config.h
@@ -21,7 +21,7 @@ char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2" };
 
 static const Rule rules[] = {
 	/* xprop(1):
